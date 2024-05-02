fac4de140cb44a2e6db8a0ed1fa788cf37d4d7ba
spar3chang3 <baker.daryk@ic.edu>
2024-05-02 01:35:36 -0500

    Pretty much finish homepage

--- Vite+React/homepage/src/App.jsx
+++ Vite+React/homepage/src/App.jsx
@@ -1,55 +1,109 @@
 import './App.css';
+import { useState } from 'react';
 
 export default function App() {
     const outNow = "Mellow";
     const outNowLink = "https://open.spotify.com/album/332eQnIev1i5PYjB866Ic7";
 
-    const redirect = (link) => {
+    const [ popoutHeight, setPopoutHeight ] = useState('0');
+    const [ arrowRotation, setArrowRotation ] = useState('0')
 
+    const redirect = (link) => {
+        window.open(link, '_blank').focus();
+    };
+
+    const handlePopout = () => {
+        switch (popoutHeight) {
+            case '0':
+                setPopoutHeight('10%');
+                setArrowRotation('-180deg');
+                break;
+            case '10%':
+                setPopoutHeight('0');
+                setArrowRotation('0');
+                break;
+        }
     }
 
     return (
-        <>
+        <div className={'homepage'}>
             <div className={'banner'}>
                 <div className={'banner-content'}>
-                    <h1>'${outNow}': Out Now!</h1>
+                    <h1>'{outNow}': Out Now!</h1>
                     <span>Exsisential Jams. Take a listen wherever you can. <a target={"_blank"} href={outNowLink}>Spotify</a> | <a target={"_blank"} href={`../articles/${outNow}.html`}>Article</a></span>
                 </div>
             </div>
             <div className={'main-content'}>
                 <div className={'button-container'}>
                     <button className={'button'}
-                            onClick={redirect("https://linktr.ee/acidfog87")}
-                            >Link Tree</button>
+                            onClick={() => redirect("https://linktr.ee/acidfog87")}
+                    >Link Tree
+                    </button>
+
+                    <div className={'qr-popout'}>
+                        <button className={'tiny-button'}
+                                onClick={() => handlePopout()}
+                        >Click to view QR Code</button>
+                        <div style={{transform: `rotate(${arrowRotation})`, transition: '100ms ease'}}>▼</div>
+                    </div>
+
+                    <img src={'http://acidfog.com/siteimage/aflt.png'} alt={"Link Tree QR Code"}
+                         style={{height: popoutHeight}}/>
 
                     <button className={'button'}
-                            onClick={redirect("https://acidfog87.bandcamp.com/")}
-                            >Bandcamp Page</button>
+                            onClick={() => redirect("https://acidfog87.bandcamp.com/")}
+                    >Bandcamp Page
+                    </button>
+
+                    <div className={'activity-buttons'}>
+                        <button className={'button'}
+                                onClick={() => redirect("./articles/ArticleList.html")}
+                        >Articles?
+                        </button>
+
+                        <button className={'button'}
+                                onClick={() => redirect("../trivia/trivia.html")}
+                        >Trivia?
+                        </button>
+
+                        <button className={'button'}
+                                onClick={() => redirect("../calendar/events.html")}
+                        >Upcoming Events?
+                        </button>
+                    </div>
 
                     <button className={'button'}
-                            onClick={redirect("./articles/ArticleList.html")}
-                            >Article List</button>
+                            onClick={() => redirect("../articles/sotm.html")}
+                    >Song of the Month
+                    </button>
 
-                    <button className={'button'}
-                            onClick={redirect("../trivia/trivia.html")}
-                            >Trivia</button>
+                    <div className={'promo-buttons'}>
+                        <button className={'button'}
+                                onClick={() => redirect("https://www.youtube.com/@AcidFogOfficial87")}
+                        >Official YouTube Channel
+                        </button>
 
-                    <button className={'button'}
-                            onClick={redirect("../calendar/events.html")}
-                            >Upcoming Events</button>
+                        <button className={'button'}
+                                onClick={() => redirect("https://www.youtube.com/@acidfogtapesofficial")}
+                        >Acid Fog Tapes Channel
+                        </button>
+                    </div>
+                </div>
 
-                    <button className={'button'}
-                            onClick={redirect("../articles/sotm.html")}
-                            >Song of the Month</button>
-
-                    <button className={'button'}
-                            onClick={redirect("https://www.youtube.com/@AcidFogOfficial87")}
-                            >Official YouTube Channel</button>
+                <div className={'spotify-plugin'}>
+                    <iframe style={{borderRadius: '12px', width: '50%'}}
+                            src="https://open.spotify.com/embed/artist/3JXrxOSYCRqrwIBF5KFNqO?utm_source=generator"
+                            width="50%" height="700" frameBorder="0" allowFullScreen=""
+                            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                            loading="lazy"></iframe>
                 </div>
             </div>
-            <div className={'footer'}>
 
+            <hr/>
+
+            <div className={'footer'}>
+                <span>If you have a suggestion, or a problem with the site, please contact us at <a href="mailto:gloomofficial87@gmail.com">our email</a></span>
             </div>
-        </>
+        </div>
     )
 }
